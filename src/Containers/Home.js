2ad4509ee6c5ed6b753a1056961f3d4205f7919e
theethawat <5910110150@psu.ac.th>
2021-03-05 16:16:17 +0700

    add line notify test

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,88 +1,92 @@
-import { Button } from "@chakra-ui/react";
-import React from "react";
-import TDCLogo from "../Assets/TDC Logo black.png";
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+import TDCLogo from '../Assets/TDC Logo black.png'
 export default function Home({ props }) {
   return (
-    <div className='container mx-auto'>
-      <div className=''>
-        <div className='flex justify-start'>
-          <img className='h-24' src={TDCLogo} alt='The Duck Creator Logo' />
+    <div className="container mx-auto">
+      <div className="">
+        <div className="flex justify-start">
+          <img className="h-24" src={TDCLogo} alt="The Duck Creator Logo" />
         </div>
 
-        <h1 className='text-2xl'>Home Page </h1>
+        <h1 className="text-2xl">Home Page </h1>
         <p>
-          Continues rest of Working by using{" "}
+          Continues rest of Working by using{' '}
           <a
-            className='text-green-600 hover:text-green-500'
-            href='https://reactjs.org'
-            alt='react website '
+            className="text-green-600 hover:text-green-500"
+            href="https://reactjs.org"
+            alt="react website "
           >
-            React{" "}
+            React{' '}
           </a>
-          ,{" "}
+          ,{' '}
           <a
-            href='https://expressjs.com'
-            className='text-green-600 hover:text-green-500'
-            alt='Express JS Webiste '
+            href="https://expressjs.com"
+            className="text-green-600 hover:text-green-500"
+            alt="Express JS Webiste "
           >
             Express
-          </a>{" "}
+          </a>{' '}
           and the other documents
         </p>
+        <a href="https://notify-bot.line.me/oauth/authorize?response_type=code&client_id=YOURCLIENTID&redirect_uri=http://localhost:7000/api/notify&scope=notify&state=test">
+          <Button size="lg"> Notify with Line</Button>
+        </a>
+
         <br />
-        <h4 className='text-xl'>ปุ่ม</h4>
-        <hr className='mb-4 mt-2' />
-        <div className='flex gap-3'>
-          <div className='flex gap-1 '>
-            <Button colorScheme='brand' size='lg'>
+        <h4 className="text-xl">ปุ่ม</h4>
+        <hr className="mb-4 mt-2" />
+        <div className="flex gap-3">
+          <div className="flex gap-1 ">
+            <Button colorScheme="brand" size="lg">
               ทดสอบ
             </Button>
-            <Button colorScheme='brand' size='md'>
+            <Button colorScheme="brand" size="md">
               ทดสอบ
             </Button>
-            <Button colorScheme='brand' size='sm'>
+            <Button colorScheme="brand" size="sm">
               ทดสอบ
             </Button>
           </div>
-          <div className='flex gap-1 font-sans font-normal'>
-            <Button colorScheme='brand' size='lg'>
+          <div className="flex gap-1 font-sans font-normal">
+            <Button colorScheme="brand" size="lg">
               ทดสอบ
             </Button>
-            <Button colorScheme='brand' size='md'>
+            <Button colorScheme="brand" size="md">
               ทดสอบ
             </Button>
-            <Button colorScheme='brand' size='sm'>
+            <Button colorScheme="brand" size="sm">
               ทดสอบ
             </Button>
           </div>
         </div>
         <br />
-        <div className='flex gap-1 '>
-          <Button colorScheme='brand' size='md'>
+        <div className="flex gap-1 ">
+          <Button colorScheme="brand" size="md">
             Primary
           </Button>
-          <Button colorScheme='pink' size='md'>
+          <Button colorScheme="pink" size="md">
             Secondary
           </Button>
-          <Button colorScheme='gray' size='md'>
+          <Button colorScheme="gray" size="md">
             Gray
           </Button>
-          <Button colorScheme='blue' size='md'>
+          <Button colorScheme="blue" size="md">
             Confirm
           </Button>
-          <Button colorScheme='red' size='md'>
+          <Button colorScheme="red" size="md">
             Danger
           </Button>
-          <Button colorScheme='yellow' size='md'>
+          <Button colorScheme="yellow" size="md">
             Warning
           </Button>
         </div>
         <br />
-        <h4 className='text-xl'>บทความ</h4>
-        <hr className='mb-4 mt-2' />
+        <h4 className="text-xl">บทความ</h4>
+        <hr className="mb-4 mt-2" />
 
-        <div className='flex flex-wrap gap-4'>
-          <div className='lg:w-1/3'>
+        <div className="flex flex-wrap gap-4">
+          <div className="lg:w-1/3">
             <p>
               เป็นมนุษย์สุดประเสริฐเลิศคุณค่า กว่าบรรดาฝูงสัตว์เดรัจฉาน
               จงฝ่าฟันพัฒนาวิชาการ อย่าล้างผลาญฤๅเข่นฆ่าบีฑาใคร
@@ -90,22 +94,22 @@ export default function Home({ props }) {
               ปฏิบัติประพฤติกฎกำหนดใจ พูดจาให้จ๊ะ ๆ จ๋า ๆ น่าฟังเอยฯ
             </p>
             <br />
-            <p className='font-normal font-sans text-lg'>
+            <p className="font-normal font-sans text-lg">
               เป็นมนุษย์สุดประเสริฐเลิศคุณค่า กว่าบรรดาฝูงสัตว์เดรัจฉาน
               จงฝ่าฟันพัฒนาวิชาการ อย่าล้างผลาญฤๅเข่นฆ่าบีฑาใคร
               ไม่ถือโทษโกรธแช่งซัดฮึดฮัดด่า หัดอภัยเหมือนกีฬาอัชฌาสัย
               ปฏิบัติประพฤติกฎกำหนดใจ พูดจาให้จ๊ะ ๆ จ๋า ๆ น่าฟังเอยฯ
             </p>
           </div>
-          <div className='lg:w-1/3'>
-            <p className='font-light font-sans text-xl'>
+          <div className="lg:w-1/3">
+            <p className="font-light font-sans text-xl">
               เป็นมนุษย์สุดประเสริฐเลิศคุณค่า กว่าบรรดาฝูงสัตว์เดรัจฉาน
               จงฝ่าฟันพัฒนาวิชาการ อย่าล้างผลาญฤๅเข่นฆ่าบีฑาใคร
               ไม่ถือโทษโกรธแช่งซัดฮึดฮัดด่า หัดอภัยเหมือนกีฬาอัชฌาสัย
               ปฏิบัติประพฤติกฎกำหนดใจ พูดจาให้จ๊ะ ๆ จ๋า ๆ น่าฟังเอยฯ
             </p>
             <br />
-            <p className='font-semibold font-sans text-2xl'>
+            <p className="font-semibold font-sans text-2xl">
               เป็นมนุษย์สุดประเสริฐเลิศคุณค่า กว่าบรรดาฝูงสัตว์เดรัจฉาน
               จงฝ่าฟันพัฒนาวิชาการ อย่าล้างผลาญฤๅเข่นฆ่าบีฑาใคร
               ไม่ถือโทษโกรธแช่งซัดฮึดฮัดด่า หัดอภัยเหมือนกีฬาอัชฌาสัย
@@ -115,5 +119,5 @@ export default function Home({ props }) {
         </div>
       </div>
     </div>
-  );
+  )
 }
